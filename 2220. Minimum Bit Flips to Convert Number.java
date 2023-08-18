98546939adfa3783e4d19b7369ae2e3911662dae
REDDY HEMANTH <107089391+ReddyHemanth4292@users.noreply.github.com>
2023-08-18 09:39:04 +0530

    xor - to find diff bit positions, AND - to find count of 1's

---
+++ 2220. Minimum Bit Flips to Convert Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int temp=start^goal;
+        int count=0;
+        while(temp>0){
+            count+=temp&1;
+            temp>>=1;
+        }
+        return count;
+    }
+}
